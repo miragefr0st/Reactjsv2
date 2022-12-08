8411b1bd803a2d79dde800e5e95b06fd94f1a493
miragefr0st <115560388+miragefr0st@users.noreply.github.com>
2022-12-08 22:54:15 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-/** URL de base pour faire une demande des films de TMDB */
+/** URL de base pour faire une demande des donnees de chaques films de TMDB */
 
 const instance = axios.create({
 	baseURL: 'https://api.themoviedb.org/3'
