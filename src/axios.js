9c0f4685bddb120d12ffb8382bfbba68e9b7989b
miragefr0st <115560388+miragefr0st@users.noreply.github.com>
2022-12-08 15:27:08 +0100

    Delete src directory

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-/** URL de base pour faire une demande des films de TMDB */
-
-const instance = axios.create({
-	baseURL: 'https://api.themoviedb.org/3'
-});
-
-export default instance;
